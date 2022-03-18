e9426e47ea2d9e476f380fdb12f94fb67100deeb
Chuanjo <chuanjo@hotmail.com>
2022-03-18 10:32:12 +0100

    empezando rutas

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,1 +1,13 @@
 const router = require("express").Router();
+const axios = require("axios");
+const MovieModel = require("../models/Movie.model");
+
+router.get("/", async (req,res,next)=>{
+    try {
+        const response = await MovieModel.find(
+            
+        )
+    } catch (error) {
+        
+    }
+})
