34620df174b2d706c2bdad6dbec4315df067255c
Chuanjo <chuanjo@hotmail.com>
2022-03-18 13:17:16 +0100

    Merge branch 'master' of https://github.com/Chuanjo/meetup-films-app-server

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -4,11 +4,12 @@ const MovieModel = require("../models/Movie.model");
 
 router.get("/", async (req,res,next)=>{
     try {
-        const response = await axios.get("https://api.themoviedb.org/3/discover/movie?api_key=ef089baa2cdc4146ef1590cfa9cc45b1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate")
-        res.json(response)
+        // const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${MOVIES_API_KEY}`)
+        // res.json(response)
+        console.log("hello")
         
     } catch (error) {
-        
+        next(error)
     }
 })
 
