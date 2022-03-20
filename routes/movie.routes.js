53996ef85569c1b08fe4c1ad21836e0a5c4e8d8b
Eduard Cabrero <eduardc.herrero@gmail.com>
2022-03-20 17:07:28 +0100

    Routes async modify

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,29 +1,53 @@
 const router = require("express").Router();
 const MovieModel = require("../models/Movie.model");
-const {moviesList, nowPlayingMovieList, nowPlayingMovieListId} = require("../service/apiService")
+const {
+  
+  nowPlayingMovieList,
+  nowPlayingMovieListId,
+  moviesGenreList,
+  moviesGenreListId,
+} = require("../service/apiService");
 
-router.get("/", async (req,res,next)=>{
-    try {
-        const response = await nowPlayingMovieList()
-        res.json(response.data)
-        
-        
-    } catch (error) {
-        next(error)
-    }
-})
+router.get("/billboard/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const response = await nowPlayingMovieListId(id);
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/billboard", async (req, res, next) => {
+  try {
+    const response = await nowPlayingMovieList();
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await moviesGenreList();
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/:id", async (req, res, next) => {
-    const { id } = req.params
-    try {
-      const response = await nowPlayingMovieListId(id)
-      console.log(response.data.results)
-      res.json(response.data.results)
-    } catch(err) {
-      next(err)
-    }
-  })
-
-
+  const { id } = req.params;
+  try {
+    const response = await moviesGenreListId(id);
+    console.log(response.data.results);
+    res.json(response.data.results);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
--- service/apiService.js
+++ service/apiService.js
@@ -1,39 +1,53 @@
 const axios = require("axios");
 
+
 const service = axios.create({
-    baseURL: `https://api.themoviedb.org/3`
-})
+  baseURL: "https://api.themoviedb.org/3",
+});
 
-const moviesList = () =>{
-    return service.get("/discover/movie", {
-        params: {
-            api_key: process.env.API_KEY,
-        }
-    })
-}
+const nowPlayingMovieList = async () => {
+  const res = await service.get("/movie/now_playing", {
+    params: {
+      api_key: process.env.API_KEY,
+    },
+  });
+  return  res;
+};
 
-const nowPlayingMovieList = () =>{
-    return service.get("/movie/now_playing", {
-        params: {
-            api_key: process.env.API_KEY,
-            
-        }
-    })
-}
+const nowPlayingMovieListId = async (id) => {
+  const res = await service.get("/movie/now_playing", {
+    params: {
+      api_key: process.env.API_KEY,
+      id,
+    },
+  });
+  return  res;
+};
 
-const nowPlayingMovieListId = (id) =>{
-    return service.get("/movie/now_playing", {
-        params: {
-            api_key: process.env.API_KEY,
-            id
-        }
-    })
-}
+const moviesGenreList = () => {
+  return service.get("/genre/movie/list", {
+    params: {
+      api_key: process.env.API_KEY,
+    },
+  });
+};
+
+const moviesGenreListId = async (id) => {
+  const res = await service.get("/movie/now_playing", {
+    params: {
+      api_key: process.env.API_KEY,
+      id,
+    },
+  });
+  return  res;
+};
 
 // "https://api.themoviedb.org/3/movie/now_playing?api_key=ef089baa2cdc4146ef1590cfa9cc45b1"
+// "https://api.themoviedb.org/3/genre/movie/list?api_key=ef089baa2cdc4146ef1590cfa9cc45b1"
 
 module.exports = {
-    moviesList,
-    nowPlayingMovieList,
-    nowPlayingMovieListId
-}
+  moviesGenreList,
+  nowPlayingMovieList,
+  nowPlayingMovieListId,
+  moviesGenreListId,
+};
